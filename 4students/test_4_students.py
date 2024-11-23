8f1ef271e190095ada8bb2352059d953054d0a90
nurgelli <nt.interpretor@gmail.com>
2024-11-23 14:04:22 +0500

    preparing quizs

--- 4students/test_4_students.py
+++ 4students/test_4_students.py
@@ -1,7 +1,29 @@
-welcome = input('Welcom, are you ready for the test (yes/no): ')
+import json
+import sys
+import random
 
-if welcome.lower() != 'yes' or 'y':
-    print('Lets jump in!')
-else:
+
+welcome = input('Welcom, are you ready for the test (yes/no): ')
+ 
+
+if welcome.lower() != 'yes':
     print('Maybe next time!')
-    quit()
+    sys.exit()
+else:
+    try:
+        with open('quiz_data.json', 'r') as file:
+            data = json.load(file)
+        print('Let\'s jump in!')
+        for q in data.values():
+            random_questions = random.sample(q, 1)
+        print(random_questions)
+    except FileNotFoundError:
+        print(f'The file was not found')
+    except json.JSONDecodeError:
+        print("The was an error decoding the JSON")
+    except Exception as e:
+        print(f'An unexpected error occured: {e}')
+
+
+
+file.close()
