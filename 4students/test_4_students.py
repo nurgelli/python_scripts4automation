22ecd08ec2f347be05d0a2137a3a26c1a97c8c12
nurgelli <nt.interpretor@gmail.com>
2024-11-24 14:17:59 +0500

    test4students done!

--- 4students/test_4_students.py
+++ 4students/test_4_students.py
@@ -2,8 +2,7 @@ import json
 import sys
 import random
 
-
-welcome = input('Welcom, are you ready for the test (yes/no): ')
+welcome = input('Welcome, are you ready for the test (yes/no): ')
  
 
 if welcome.lower() != 'yes':
@@ -13,27 +12,24 @@ else:
     try:
         with open('quiz_data.json', 'r') as file:
             data = json.load(file)
-        print('Let\'s jump in!')
+        # print('Let\'s jump in!')
         
         def get_quiz():
             questions = data['questions']
             random.shuffle(questions)
             selected_question = questions[:1]
-            score = 0
+                                    
             for question in selected_question:
-                print(question['question'])
+                print(f'\n{question['question']}')
+                
                 for option in question['options']:
                     print(option)
-                student_answer = input("Your answer (just enter the Letter): ").strip().upper()
-                if student_answer != question['answer']:
-                    print('Wrong answer, try again!')
-                    sys.exit()
-                else:
-                    print("Well done!")
-                    score += 1
-                    
                 
-            print(f'Your score: {score}/1')
+                student_answer = input("Your answer (just choice letter): ").strip().upper()
+                if student_answer == question['answer']:
+                    print('\n Well done!')
+                else:
+                    print('\n Wrong answer!')                
     except FileNotFoundError:
         print(f'The file was not found')
     except json.JSONDecodeError:
@@ -41,7 +37,14 @@ else:
     except Exception as e:
         print(f'An unexpected error occured: {e}')
 
+file.close()
+
 while True:
     get_quiz()
-
-file.close()
+    retry = input().strip()
+    if retry == '':
+        print("Pressed Enter! Continuing...")
+    elif retry == 'exit':
+        print("Exiting the program. Goodby!")
+        break
+        
