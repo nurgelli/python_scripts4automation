bb8e3803f96635b698ec5b260e89afa37b7af1e0
nurgelli <nt.interpretor@gmail.com>
2024-11-26 22:37:25 +0500

    bash extension change script added

--- bash_script/retry.sh
+++ bash_script/retry.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 
-n=0
-command=$1
-while ! $command && [ $n -le 5 ]; do
-    sleep $n
-    ((n=n+1))
-    echo "Retry #$n"
-done;
+for file in *.docx; do
+    name=$(basename "$file" .dox)
+    mv "$file" "$name.doc"
+done
