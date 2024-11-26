f06273ba085f61ca4d104e05a14ad41f6ea86cba
nurgelli <nt.interpretor@gmail.com>
2024-11-26 10:54:09 +0500

    new approach for words synonyms

--- 4students/word_synonyms.py
+++ 4students/word_synonyms.py
@@ -2,9 +2,12 @@
 import requests
 
 
-res = requests.get('https://api.datamuse.com/words')
+res = requests.get('https://api.datamuse.com/words?ml=duck')
 data = res.json()
 print(data)
-
+for n, i in enumerate(data, 1):
+    print(n, i["word"])
+    
+    
 
 
