460c3131736a65b1d9872ac485dff169119419dc
nurgelli <nt.interpretor@gmail.com>
2024-11-27 15:36:38 +0500

    added reversed word

---
+++ 4students/reversed_words.py
@@ -0,0 +1,12 @@
+
+
+def reversed_words():
+   word_inp = input("Enter word that want to reverse: ").strip().lower()
+   reversed_word = word_inp[::-1]
+   print(reversed_word)
+   return reversed_word
+
+
+reversed_words()
+   
+    
