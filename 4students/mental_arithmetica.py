27942bd443387d841a7a30bd4ccdad995389ffac
nurgelli <nt.interpretor@gmail.com>
2024-11-27 13:49:25 +0500

    fixed some bugs

--- 4students/mental_arithmetica.py
+++ 4students/mental_arithmetica.py
@@ -3,28 +3,33 @@ import time
 import sys
 
 
-def display_and_sum(count, interval):
-    total_sum = 0
-    for i in range(count):
-        number = random.randint(1,10)
-        print(f'{number}')
-        total_sum += number
-        time.sleep(interval)
-        
-    return total_sum
+
 
 def main():
     
-    range_inp = input('Please enter count of displaying number: ').strip()
-    interval_inp = input('Please enter the interval time between each displaying of numbers: ').strip()
+    range_inp = input('Range please: ').strip() 
+    start_inp = input('Are you ready? yes/no: ').strip().lower() 
     
-    start_inp = input('Are you ready? yes/no: ').strip().lower()
+    
+    def display_and_sum(count, interval):
+        total_sum = 0
+        for i in range(count):
+            number = random.randint(1, int(range_inp))
+            print(f'{number}')
+            total_sum += number
+            time.sleep(interval)
+            
+        return total_sum
+    
+    
     if start_inp == 'yes':
         time.sleep(4)
-        call_func = display_and_sum(int(range_inp), int(interval_inp))
+        call_func = display_and_sum(10, 2)
         print(f'Correct answer is: {call_func}')
     else:
         print("Maybe, next time!")
         sys.exit()
+        
+        
   
 main()
